df6db6a0184306fc386ba81062cc95864c40d881
yc <254232364@qq.com>
2018-10-29 23:57:40 +0800

    文件提交

---
+++ application/api/controller/Index.php
@@ -0,0 +1,42 @@
+<?php
+// +----------------------------------------------------------------------
+// |  [ 我的梦想是星辰大海 ]
+// +----------------------------------------------------------------------
+// | Author: yc  and yc@yuanxu.top
+// +----------------------------------------------------------------------
+// | Date: 2018/10/29 Time: 22:56
+// +----------------------------------------------------------------------
+
+namespace app\api\controller;
+use think\Controller;
+
+class Index extends Controller {
+
+    public function index()
+    {
+
+        $data  = input('post.');
+
+        if (!$data){
+            echo 'false';
+            return false;
+        }
+
+        $json = json_encode($data);
+        file_put_contents('a.log',$json);
+        return view(__FUNCTION__);
+    }
+
+    public function add(){
+//        $data  = input('post.');
+
+        $data = '[{"username":"real999111","content":"龙神牛逼，lssb","createtime":1540825809},{"username":"晚风","content":"没控制出纷争","createtime":1540825810},{"username":"专注√","content":"主要是前期先知发条节奏太差","createtime":1540825811},{"username":"robben","content":"现在知道菜了吧，英雄勺不会玩核玩啥单一征召呢","createtime":1540825811},{"username":"飞鹰","content":"缺龙心啊","createtime":1540825811},{"username":"空空如也","content":"1","createtime":1540825812},{"username":"木子","content":"不是说好了三红随便喷","createtime":1540825812},{"username":"德雷克","content":"不懂你们喷啥，你们没看懂吗，就是阵容打不过","createtime":1540825813},{"username":"空空如也","content":"1","createtime":1540825813},{"username":"Graham0","content":"守护全世界最可爱的龙公明哥哥","createtime":1540825813}]';
+
+        if (!$data){
+            echo 'false';
+            return false;
+        }
+
+    }
+
+}
