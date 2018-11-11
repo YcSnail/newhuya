94a8540149f723e25ebe193e2f14590f1b4d14aa
yc <254232364@qq.com>
2018-11-12 00:39:39 +0800

    新增礼物

--- application/api/controller/Danmu.php
+++ application/api/controller/Danmu.php
@@ -12,6 +12,52 @@ use think\Controller;
 
 class Danmu extends Controller {
 
+    public function addGift(){
+
+        $dataArr  = input('post.');
+
+        if (!$dataArr){
+            ajaxRes(-1,'data is empty!');
+        }
+
+        if (!isset($dataArr['gift'])){
+            ajaxRes(-1,'gift is empty!');
+        }
+
+        $giftArr = $dataArr['gift'];
+        $setArr = array();
+
+        $UserModel = model('User');
+
+        // 循环礼物列表
+        for ($i=0;$i<count($giftArr);$i++) {
+
+            $tmpArr = [];
+            // 弹幕过滤检测
+            $tmpArr['name'] = $giftArr[$i]['giftName'];
+            $tmpArr['count'] = $giftArr[$i]['count'];
+            $tmpArr['price'] = $giftArr[$i]['price'];
+
+            $tmpArr['userid'] = $UserModel->getUserId($giftArr[$i]['username']);
+            $tmpArr['create_time'] = $giftArr[$i]['createtime'];
+            $setArr[] = $tmpArr;
+
+        }
+
+        if ($setArr){
+
+            $giftModel = model('Gift');
+            $giftRes = $giftModel->addgift($setArr);
+            if ($giftRes){
+                ajaxRes(0,'ok');
+            }
+
+        }
+
+        ajaxRes(-1,'insert Error');
+    }
+
+
     public function add(){
 
         $dataArr  = input('post.');
@@ -69,13 +115,16 @@ class Danmu extends Controller {
      */
     private function checkDanmu($content){
 
+        if (empty($content)){
+            return false;
+        }
+
         if (strlen($content) <=3){
             return false;
         }
 
-
         $checkArr = ['分享了直播间','hahaha','哈哈哈','2333','6666','牛逼','收','高能预警',
-            '前方高能反应','????','....','。。。。','支付宝','QQ','qq','？？？？','3333','¿¿¿¿','/{'];
+            '前方高能反应','????','....','。。。。','支付宝','QQ','qq','？？？？','3333','¿¿¿¿','/{','激动的心'];
 
         $resCount = $content;
 
